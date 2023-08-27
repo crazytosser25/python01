b3e76fd8206a1d483e5b748eae56b23c9b6c166f
Yevhenii_Baranov <jask.b@outlook.com>
2023-08-27 13:07:31 +0200

    calc commit

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello world.")
-print("Hello Git")
+result = None
+operand = None
+operator = None
+wait_for_number = True
+
+while True:
+   try:
+       operand = int(input())
+   except ValueError:
+       print(f'{operand} is not a number. Try again.')
+       operand = int(input())
+    if result = None:
+        result = operand
+    wait_for_number = False
+    try:
+        operator = input()
+        operator = int(operator)
+    except ValueError:
+        continue
+    else:
+        print(f'{operator} is not '+' or '-' or '/' or '*'. Try again')
+        operator = input()
